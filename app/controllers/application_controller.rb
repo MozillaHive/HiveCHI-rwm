94b9698381d273891f3f087f63634b16948d926e
timchoh585 <timchoh@gmail.com>
2015-06-06 12:50:00 -0500

    fixing merge conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-end
-=======
+  
   helper_method :current_user
   def current_user
       @current_user ||= User.find_by_id(session[:user])
   end
 end
->>>>>>> f7d09b956e8bf04d2fb90f972ef1b67ba0cfe50e
