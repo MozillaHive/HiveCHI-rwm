b6d899aa342a59af726268a993c0a3387fe8a65b
Chris Bohl <chrisrbohl@gmail.com>
2015-06-06 13:39:55 -0500

    Added sessions controller methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,28 @@
 class SessionController < ApplicationController
-  def login
+  def new
+    @user = User.new
+    render '_login'
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      @errors = ["Invalid email or password"]
+      render '_login'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+
+  private
+    def user_params
+     params.require(:user).permit(:email, :password)
+    end
   end
 end
