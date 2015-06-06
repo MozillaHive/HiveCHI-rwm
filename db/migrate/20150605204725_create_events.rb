e9f0e500531045000a1067aeead148028e0f35cc
Jeffrey <subbaraojeffrey@gmail.com>
2015-06-06 13:08:18 -0500

    Seeding

--- db/migrate/20150605204725_create_events.rb
+++ db/migrate/20150605204725_create_events.rb
@@ -7,6 +7,7 @@ class CreateEvents < ActiveRecord::Migration
       t.float  :duration
       t.string  :description
       t.belongs_to  :organization
+      t.string :type
 
 
       t.timestamps null: false
