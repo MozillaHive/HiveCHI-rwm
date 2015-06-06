4281c8614974c8136e3315fdd84cb806e11f3472
timchoh585 <timchoh@gmail.com>
2015-06-06 18:44:27 -0500

    editing pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,15 +5,15 @@ class EventsController < ApplicationController
       event.as_json.merge(:numberOfAttendees => event.attendances.count)
     end
 
-    preferences_json = {
-      preference_1: User.find_by_id(session[:user_id]).preference_1,
-      preference_2: User.find_by_id(session[:user_id]).preference_2,
-      preference_3: User.find_by_id(session[:user_id]).preference_3
-    }
+    #preferences_json = {
+    #  preference_1: User.find_by_id(session[:user_id]).preference_1,
+    #  preference_2: User.find_by_id(session[:user_id]).preference_2,
+    #  preference_3: User.find_by_id(session[:user_id]).preference_3
+    #}
 
-    render json: {
-      :events => events_json,
-      :preferences => preferences_json
-    }
+    #render json: {
+    #  :events => events_json,
+    #  :preferences => preferences_json
+    #}
   end
 end
