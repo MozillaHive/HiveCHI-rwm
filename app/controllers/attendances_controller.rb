f2f3a43754a6b928cde9b8d67bf867bacbfb7a2d
Chris Bohl <chrisrbohl@gmail.com>
2015-06-06 18:52:22 -0500

    writing first json object

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,6 +4,26 @@ class AttendancesController < ApplicationController
       schoolAddress: User.find_by(id: session[:user_id]).school.address,
       eventAddress: Event.find_by_id(params[:event_id]).address
     }
-    render json: addresses_json
+
+    File.open("public/addresses.json","w") do |f|
+      f.write(addresses_json.to_json)
+    end
+    # render json: addresses_json
+
+  #     events_json = []
+  # Event.all.each do |event|
+  #   event_json = {
+  #     "id" => event.id,
+  #     "start" => event.start_at,
+  #     "end" => event.end_at,
+  #     "title" => event.name,
+  #     "body" => event.event_description,
+  #     "status" => event.status
+  #   }
+  #   events_json << event_json
+  # end
+  # File.open("public/event.json","w") do |f|
+  #   f.write(events_json.to_json)
+  # end
   end
 end
