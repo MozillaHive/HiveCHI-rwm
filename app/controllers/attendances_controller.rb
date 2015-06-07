a4126b12cd3cfbf1f88a26ea596273a73a3d0b2b
Chris Bohl <chrisrbohl@gmail.com>
2015-06-07 12:37:50 -0500

    necessary changes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,9 @@
 class AttendancesController < ApplicationController
   def new
-   addresses_json = {
-      schoolAddress: current_user.school.address,
-      eventAddress: Event.find_by_id(params[:event_id]).address
-    }
-    render json: addresses_json
+   # addresses_json = {
+   #    schoolAddress: current_user.school.address,
+   #    eventAddress: Event.find_by_id(params[:event_id]).address
+   #  }
+   #  render json: addresses_json
   end
 end
