b1444c05e73856e0e89cd194fc6f6c3c019001d4
Jeffrey <subbaraojeffrey@gmail.com>
2015-06-11 22:10:13 -0500

    Calculates school atendees for trending and displays school attendees in trending and on the event description page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 	#layout false
   def index
-    sleep(2.0)
+  #  sleep(2.0)
     if session[:user_id]
       redirect_to :action => "dashboard", :controller => "welcome"
     else
@@ -11,5 +11,15 @@ class WelcomeController < ApplicationController
 
   def dashboard
       @user_events = User.find(session[:user_id]).events_attended
+      school = User.find(session[:user_id]).school
+      @hot_events = Hash.new(0)
+      school.students.each do |s|
+        s.events_attended.each do |e|
+          @hot_events[e] +=1
+        end
+      end
+      @events_arr = @hot_events.sort_by{|e,n| n}
+      @events_arr.reverse!
+      @hot_events_num = 5
   end
 end
