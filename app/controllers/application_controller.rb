d1f66d28d68398601de86064b49f6deec40cbaa9
Chris Bohl <chrisrbohl@gmail.com>
2015-06-23 20:23:37 -0500

    testing gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
+
   helper_method :current_user
   def current_user
       @current_user ||= User.find_by_id(session[:user])
