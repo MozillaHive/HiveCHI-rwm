8b9a635a1af00baca456f83f3e365b420e1e5bca
Chris Bohl <chrisrbohl@gmail.com>
2015-06-23 20:25:08 -0500

    fixed space problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   helper_method :current_user
   def current_user
       @current_user ||= User.find_by_id(session[:user])
