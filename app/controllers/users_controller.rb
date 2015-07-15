efefd2c2a84e15a00aaaba683ce17900fb7279ac
Kevin Yaroch <kjyaroch@gmail.com>
2015-07-15 15:38:30 -0500

    Minor formatting change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,8 +53,7 @@ class UsersController < ApplicationController
   private
   def user_params
      params.require(:user).permit(
-      :username, :email, :password, :password_confirmation, :phone,
-      :school_id
+      :username, :email, :password, :password_confirmation, :phone, :school_id
      )
   end
 end
