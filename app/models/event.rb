9f9719c1e519ea6e12b0ddcb0107ee819fa1f41f
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-01 00:12:48 -0500

    Add methods to find popular events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,26 @@ class Event < ActiveRecord::Base
   belongs_to  :organization
   has_many    :attendances
   has_many    :attendees , through: :attendances, source: :user
+
+  def self.popular_events_by_school(school, number)
+    popular_events = Event.future_events.sort_by do |event|
+      -event.attendees.select { |attendee| attendee.school == school }.size
+    end
+    popular_events[0...number]
+  end
+
+  def self.popular_events(number)
+    Event.future_events.sort_by { |event| -event.attendees.count }[0...number]
+  end
+
+  def self.future_events
+    Event.all.select do |event|
+      (event.start_date_and_time + event.duration).future?
+    end
+  end
+
+  def attendees_by_school(school)
+    self.attendees.select { |attendee| attendee.school == school }.count
+  end
+
 end
