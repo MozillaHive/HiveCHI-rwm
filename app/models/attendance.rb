e1a2cd6a0c690acc96d01c668bb0f0f2b7aa9633
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-02 17:24:11 -0500

    Also validate presence of user_id, event_id

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,5 @@ class Attendance < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :event
   validates_uniqueness_of :user_id, scope: :event_id
+  validates_presence_of :user_id, :event_id
 end
