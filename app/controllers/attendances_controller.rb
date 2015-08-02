4faa9a30fa103fc1418578990482e78648643481
Jeffrey <subbaraojeffrey@gmail.com>
2015-08-02 17:40:49 -0500

    reformated map page and adden attendance based on db

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -7,7 +7,11 @@ class AttendancesController < ApplicationController
       redirect_to "/redirect"
     end
     @event = Event.find(params[:event_id])
-
+    @attend_hash = {transit: [0,0,0], walking: [0,0,0], bicycling: [0,0,0]}
+    @event.attendances.each do |a|
+    if (a.commitment_status == "Yes" and a.user.school_id == current_user.school_id)
+      @attend_hash[a.method_of_transit][index_from_dep_time(a.departure_type)] += 1
+    end
   end
 
   def create
@@ -30,4 +34,16 @@ class AttendancesController < ApplicationController
   	event = Event.find(params[:event_id])
     @attendance = Attendance.find(params[:id])
   end
+
+  def index_from_dep_time(dep_time)
+    if dep_time == "Early"
+      return 0
+    elsif dep_time == 'On Time'
+      return 1
+    else
+      return 2
+    end
+  end
+end
+
 end
