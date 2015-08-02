9521cfb3ed6b9657b3e203a5b16a5b238f5ff79a
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-02 18:48:23 -0500

    Add methods and view to send password reset email

---
+++ app/controllers/password_reset_controller.rb
@@ -0,0 +1,14 @@
+class PasswordResetController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    flash[:notice] = "Your password has been reset. Please follow the " \
+                     "instructions in the email we sent you."
+    @user.reset_password! if @user
+    redirect_to login_path
+  end
+
+end
