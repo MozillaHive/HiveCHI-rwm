416c4f62b8348b126909d3ad916607161c506f4e
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-05 14:43:27 -0500

    Add spec for visiting event page when logged in

--- spec/features/event_join_spec.rb
+++ spec/features/event_join_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature 'User joins an event', js: true do
-  before { create(:event) }
+  before { @event = create(:event) }
 
   scenario "User visits event page when not logged in" do
-    puts Event.all.count
-    visit event_path(Event.first)
+    visit event_path(@event)
     expect(page).not_to have_button("Join")
     expect(page).not_to have_button("Watch")
     expect(page).not_to have_button("Back Out")
     expect(page).not_to have_content("Number attending from your school")
     expect(page).to have_button("Log in")
+  end
+
+  scenario "User visits event page when logged in" do
+    log_in
+    visit event_path(@event)
+    expect(page).to have_button("Join")
+    expect(page).to have_button("Watch")
+    expect(page).not_to have_button("Back Out")
+    expect(page).to have_content("Number attending from your school")
+    expect(page).not_to have_button("Log in")
   end
 end
