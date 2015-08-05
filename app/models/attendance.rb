6b3fe39cc1d3efeb35dbb019cfa2ccfee306db2d
Jeffrey <subbaraojeffrey@gmail.com>
2015-08-05 09:37:16 -0500

    Parents can watch events for their kids. This adds them to the watch list and texts the student user

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,7 +7,7 @@ class Attendance < ActiveRecord::Base
   def description
     case self.commitment_status
     when "Yes" then "You are going to this event"
-    when "Maybe" then "You might be going to this event"
+    when "Maybe" then "You are watching this event"
     when "No" then "You backed out of this event"
     end
   end
