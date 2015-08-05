d1c1b883bfa054520a3affeed8b263e896517aea
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-05 15:48:18 -0500

    add event join spec

--- spec/features/event_join_spec.rb
+++ spec/features/event_join_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature 'User joins an event', js: true do
+RSpec.feature 'User interacts with an event', js: true do
   before { @event = create(:event) }
 
-  scenario "User visits event page when not logged in" do
-    visit event_path(@event)
-    expect(page).not_to have_button("Join")
-    expect(page).not_to have_button("Watch")
-    expect(page).not_to have_button("Back Out")
-    expect(page).not_to have_content("Number attending from your school")
-    expect(page).to have_button("Log in")
+  context "When user is not logged in" do
+    scenario "user visits events page" do
+      visit event_path(@event)
+      expect(page).not_to have_button("Join")
+      expect(page).not_to have_button("Watch")
+      expect(page).not_to have_button("Back Out")
+      expect(page).not_to have_content("Number attending from your school")
+      expect(page).to have_button("Log in")
+    end
   end
 
-  scenario "User visits event page when logged in" do
-    log_in
-    visit event_path(@event)
-    expect(page).to have_button("Join")
-    expect(page).to have_button("Watch")
-    expect(page).not_to have_button("Back Out")
-    expect(page).to have_content("Number attending from your school")
-    expect(page).not_to have_button("Log in")
+  context "When user is logged in" do
+    before { log_in }
+
+    scenario "user visits events page" do
+      visit event_path(@event)
+      expect(page).to have_button("Join")
+      expect(page).to have_button("Watch")
+      expect(page).not_to have_button("Back Out")
+      expect(page).to have_content("Number attending from your school")
+      expect(page).not_to have_button("Log in")
+    end
+
+    scenario "user joins an event" do
+      visit event_path(@event)
+      click_button "Join"
+      expect(page).not_to have_button("Join")
+      expect(page).to have_button("Back Out")
+      expect(page).to have_content("You are going to this event")
+    end
   end
 end
