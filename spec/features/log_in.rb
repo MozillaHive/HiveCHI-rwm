680a5869b4181a763088039c6e96eae32d9acd97
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-05 12:24:21 -0500

    Add successful registration test

--- spec/features/log_in.rb
+++ spec/features/log_in.rb
@@ -12,4 +12,22 @@ RSpec.feature 'User logs in' do
     expect(page).to have_content('Logout')
   end
 
+  scenario "With invalid password" do
+    visit login_path
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: "password1235"
+    click_button 'Log in'
+    expect(page).not_to have_content('Dashboard')
+    expect(page).to have_content('Invalid username or password')
+  end
+
+  scenario "With invalid username" do
+    visit login_path
+    fill_in 'Username', with: "usernameisfake"
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).not_to have_content('Dashboard')
+    expect(page).to have_content('Invalid username or password')
+  end
+
 end
