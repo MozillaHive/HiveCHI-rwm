4efb20b5615066f828914f15643b644f07fb01c7
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-08 12:34:05 -0500

    Make text message dependent on creation of nudge

--- app/controllers/nudges_controller.rb
+++ app/controllers/nudges_controller.rb
@@ -8,11 +8,18 @@ class NudgesController < ApplicationController
 	end
 
 	def create
-		nudge = Nudge.create(nudger: User.find(session[:user_id]), nudgee: User.find(params[:nudgee]),event: Event.find(params[:id]))
-		text_message(nudge)
-		flash[:notice] = "You nudged #{nudge.nudgee.username} to go to #{nudge.event.name}"
-		nudge.save
-		client_redirect "/dashboard"
+		@nudge = Nudge.new(
+			nudger: User.find(session[:user_id]),
+			nudgee: User.find(params[:nudgee]),
+			event: Event.find(params[:id])
+		)
+		if @nudge.save
+			@nudge.send_text!
+			flash[:notice] = "You nudged #{nudge.nudgee.username} to go to #{nudge.event.name}"
+			redirect_to dashboard_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -34,18 +41,5 @@ class NudgesController < ApplicationController
 			redirect_to @event
 		end
 	end
-
-	def text_message(nudge)
-		account_sid = Rails.application.secrets.twilio_sid
-		auth_token = Rails.application.secrets.twilio_auth_token
-		client = Twilio::REST::Client.new account_sid, auth_token
-		from = "+18443117433" # Your Twilio number\
-
-		client.account.messages.create(
-    		:from => from,
-    		:to => nudge.nudgee.phone,
-    		:body => "Hey #{nudge.nudgee.username}, #{nudge.nudger.username} wants to go to #{nudge.event.name} if you'll go too! Reply at #{request.base_url+"/events/"+params[:id]}"
-  		)
-	end
-
+	
 end
--- app/models/nudge.rb
+++ app/models/nudge.rb
@@ -4,6 +4,18 @@ class Nudge < ActiveRecord::Base
 	belongs_to :event
 	validate :allowed_to_send?
 
+	def send_text!
+		account_sid = Rails.application.secrets.twilio_sid
+		auth_token = Rails.application.secrets.twilio_auth_token
+		client = Twilio::REST::Client.new account_sid, auth_token
+		from = "+18443117433" # Your Twilio number
+		client.account.messages.create(
+				:from => from,
+				:to => self.nudgee.phone,
+				:body => "Hey #{self.nudgee.username}, #{self.nudger.username} wants to go to #{self.event.name} if you'll go too! Reply at http://#{ENV['HOSTNAME']}/events/#{self.event.id}"
+			)
+	end
+	
 	private
 
 	def allowed_to_send?
