08381a383a1580cdb9bd93e7a55282054ebda9cb
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-07 22:22:42 -0500

    Working form checkbox

--- app/models/nudge.rb
+++ app/models/nudge.rb
@@ -2,4 +2,14 @@ class Nudge < ActiveRecord::Base
 	belongs_to :nudger, class_name: "User", foreign_key: "nudger_id"
 	belongs_to :nudgee, class_name: "User", foreign_key: "nudgee_id"
 	belongs_to :event
+	validate :allowed_to_send?
+
+	private
+
+	def allowed_to_send?
+		unless self.nudgee.nudges_enabled
+			errors.add(:nudgee, "has chosen not to receive nudges")
+		end
+	end
+
 end
