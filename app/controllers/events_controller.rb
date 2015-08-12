5c8ecd705cebf9d435617c57730941d88ca88ea3
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-12 12:52:20 -0500

    Remove puts statements from EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,6 @@ class EventsController < ApplicationController
   end
 
   def today
-    puts "TODAY"
     events = Event.where('start_date_and_time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all
 
     @events_json = events.map do |event|
@@ -28,7 +27,6 @@ class EventsController < ApplicationController
   end
 
   def tomorrow
-    puts "TOMORROW"
     events = Event.where('start_date_and_time BETWEEN ? AND ?', DateTime.now.beginning_of_day + 1.days, DateTime.now.end_of_day + 1.days).all
 
     @events_json = events.map do |event|
@@ -41,7 +39,6 @@ class EventsController < ApplicationController
   end
 
   def this_week
-    puts "THISWEEK"
     events = Event.where('start_date_and_time BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day + 6.days).all
 
     @events_json = events.map do |event|
@@ -75,9 +72,6 @@ class EventsController < ApplicationController
     @out_nudge = Nudge.find_by(event: @event, nudger_id: session[:user_id])
     @in_nudge = Nudge.find_by(event: @event, nudgee_id: session[:user_id])
     flash[:attendance] = @attend.id if @attend
-    puts @attend
-    puts @out_nudge
-    puts @in_nudge
   end
 
   def store_user_commitment
