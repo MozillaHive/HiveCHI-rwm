96f4534a35a10c5750f5cc8d2e803bc386910f1d
derekgilwa <dgliwa@thoughtworks.com>
2015-08-12 19:56:10 -0500

    Derek - add more robust test suite.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,12 +3,15 @@ class Attendance < ActiveRecord::Base
   belongs_to  :event
   validates_uniqueness_of :user_id, scope: :event_id
   validates_presence_of :user_id, :event_id
+  YES = "Yes"
+  NO = "No"
+  MAYBE = "Maybe"
 
   def description
     case self.commitment_status
-    when "Yes" then "You are going to this event"
-    when "Maybe" then "You are watching this event"
-    when "No" then "You backed out of this event"
+    when YES then "You are going to this event"
+    when MAYBE then "You are watching this event"
+    when NO then "You backed out of this event"
     end
   end
 
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,11 @@
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+null_stream = IO.new IO.sysopen('/dev/null', 'w+')
+Capybara.register_driver :poltergeist do |app|
+    capybara_options = { phantomjs_logger: null_stream, js_errors: false }
+    Capybara::Poltergeist::Driver.new(app, capybara_options)
 end
+
+Capybara.javascript_driver = :poltergeist
