81bd4e963a4e853ec77793eb443856df818e9b0f
Kevin Yaroch <kjyaroch@gmail.com>
2015-08-25 20:08:14 -0500

    Finish migrations to move student data

--- db/migrate/20150826005144_move_student_data.rb
+++ db/migrate/20150826005144_move_student_data.rb
@@ -1,6 +1,6 @@
 class MoveStudentData < ActiveRecord::Migration
   def change
-    execute "INSERT INTO students (username, school_id, address, nudges_enabled)" \
-              "SELECT (username, school_id, address, nudges_enabled) FROM users;"
+    execute "INSERT INTO students (username, school_id, address, nudges_enabled, created_at, updated_at)" \
+              " SELECT username, school_id, home_address, nudges_enabled, created_at, updated_at FROM users;"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826004331) do
+ActiveRecord::Schema.define(version: 20150826005519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,8 +80,9 @@ ActiveRecord::Schema.define(version: 20150826004331) do
     t.string   "username"
     t.integer  "school_id"
     t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.boolean  "nudges_enabled"
   end
 
   add_index "students", ["username"], name: "index_students_on_username", unique: true, using: :btree
