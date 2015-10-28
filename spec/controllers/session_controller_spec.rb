bca39b18346abe4962470c98a11f32fa4158b3c2
Kevin Yaroch <kjyaroch@gmail.com>
2015-10-28 12:16:21 -0500

    Redirect admins to admin path on login

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe SessionController do
   let(:student) { create(:verified_student) }
   let(:service_provider) { create(:verified_service_provider) }
+  let(:admin) { create(:admin) }
 
   describe "GET #new" do
     before { get :new }
@@ -10,15 +11,7 @@ RSpec.describe SessionController do
   end
 
   describe "POST #create" do
-    context "as student" do
-      context "with valid username and password" do
-        before do
-          post :create, session: { username: student.username, password: "password1234" }
-        end
-        specify { expect(response).to redirect_to(dashboard_path) }
-        specify { expect(session[:user_id]).to eq(student.user.id) }
-      end
-
+    context "with invalid information" do
       context "with invalid username" do
         before do
           post :create, session: { username: "doesntexist", password: "password1234" }
@@ -36,14 +29,28 @@ RSpec.describe SessionController do
       end
     end
 
-    context "as service provider" do
-      context "with valid username and password" do
-        before do
-          post :create, session: { username: service_provider.user.email, password: "password1234" }
-        end
-        specify { expect(response).to redirect_to(service_provider_root_path) }
-        specify { expect(session[:user_id]).to eq(service_provider.user.id) }
+    context "as student" do
+      before do
+        post :create, session: { username: student.username, password: "password1234" }
       end
+      specify { expect(response).to redirect_to(dashboard_path) }
+      specify { expect(session[:user_id]).to eq(student.user.id) }
+    end
+
+    context "as service provider" do
+      before do
+        post :create, session: { username: service_provider.user.email, password: "password1234" }
+      end
+      specify { expect(response).to redirect_to(service_provider_root_path) }
+      specify { expect(session[:user_id]).to eq(service_provider.user.id) }
+    end
+
+    context "as admin" do
+      before do
+        post :create, session: { username: admin.user.email, password: "password1234" }
+      end
+      specify { expect(response).to redirect_to(rails_admin_path) }
+      specify { expect(session[:user_id]).to eq(admin.user.id) }
     end
   end
 
