e221912a828fd21ab8eb8da434519d886141ab36
Kevin Yaroch <kjyaroch@gmail.com>
2016-01-23 08:36:56 -0600

    Fix other SP regex

--- app/validators/service_provider_domain_validator.rb
+++ app/validators/service_provider_domain_validator.rb
@@ -1,7 +1,7 @@
 class ServiceProviderDomainValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if record.role_type == "ServiceProvider"
-      domain_match = value.match(/@.+\z/)
+      domain_match = value.match(/@(.+)\z/)[1]
       unless domain_match && Organization.find_by(domain_name: domain_match[0])
         record.errors[attribute] << "does not belong to a registered organization"
       end
