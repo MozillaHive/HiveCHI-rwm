4b0d337a5fd7f3e7b6eebd4e34149a2fab04dbc8
Kevin Yaroch <kjyaroch@gmail.com>
2016-02-22 17:08:48 -0600

    Fix typo that caused admin model specs to fail

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   it "is invalid without a user" do
-    expect(build(:parent, user_attributes: {})).not_to be_valid
+    expect(build(:admin, user_attributes: {})).not_to be_valid
   end
 end
